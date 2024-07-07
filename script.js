ceb80409e3a70a553da9b28bf162de5f911ac775
Surve <surve8729@gmail.com>
2024-07-08 00:41:41 +0530

    Add Display and Buttons in html

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1/num2;
 }
+
+function operate(operator, num1, num2){
+    // console.log(typeof(operator), num1, num2);
+    if(operator === '+'){
+        return add(num1, num2);
+    }
+    if(operator === '-'){
+        return subtract(num1, num2);
+    }
+    if(operator === '*'){
+        return multiply(num1, num2);
+    }
+    if(operator === '/'){
+        return divide(num1, num2);
+    }
+}
+
+let number1 = 0;
+let number2 = 0;
+let operator = '';
+
+console.log('Hello world')
+
+console.log(operate('+', 2, 2));    // 4
+console.log(operate('-', 2, 2));    // 0
+console.log(operate('*', 2, 5));    // 10
+console.log(operate('/', 10, 2));    // 5
