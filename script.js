560a6c2e49727329df25abe405782aef2c5c2530
Surve <surve8729@gmail.com>
2024-07-08 00:13:42 +0530

    Add functions for basic arithmetic operations

---
+++ script.js
@@ -0,0 +1,21 @@
+// add
+// subtract
+// multiply
+// divide
+
+
+function add(num1, num2){
+    return num1+num2;
+}
+
+function subtract(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+}
