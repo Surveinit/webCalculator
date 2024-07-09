830fcb1b3f607b8b9a726def26d57f89b3740d52
Surve <surve8729@gmail.com>
2024-07-10 00:30:10 +0530

    Add new font to displayContent & hover on buttons

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
     return Math.round((num1/num2) * 100) / 100;
-}
+}   
 
 function operate(operator, num1, num2){
     // console.log(typeof(operator), num1, num2);
